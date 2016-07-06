248cbe07c9eb07a228b1ebe7d62b798fbd3cb4f0
DerBene <pvpbene@googlemail.com>
2016-07-06 16:06:17 +0200

    added newsletter db entry

--- datenverarbeitung.php
+++ datenverarbeitung.php
@@ -9,12 +9,58 @@
 	$nachname = $_POST["nachnameInput"];
 	$email = $_POST["emailInput"];
 	
+	if (substr_count($email,"@") != 1)
+            {echo "<p>Ist keine Emailadresse</p>";
+            exit;
+            }
+			
+	if (substr_count($vorname,"") != 1)
+            {echo "<p>Ist keine name</p>";
+            exit;
+            }
+	if (substr_count($vorname,"") != 1)
+            {echo "<p>Ist keine name</p>";
+            exit;
+            }
+	
 	echo "<h2>Empf&auml;nger:&nbsp" .$email. "</h2><br>";
 	echo "Sehr geehrte(r) Frau/Herr&nbsp;" .$vorname."&nbsp;". $nachname. ", <br>";
 	echo "um Ihre Anmeldung f&uuml;r unseren Newsletter abzuschlie&szlig;en, klicken sie bitte auf den folgenden Link: &nbsp <a href='bhwebhome.html'>link</a><br><br> Falls Sie Unseren Newsletter nicht abonniert haben sollten, ignorieren sie diese e-mail.<br>";
 	echo "<br>";
 	echo "Mit freundlichen Gr&uuml;&szlig;en<br>";
 	echo "Ihr Brainhouse Team";
+	
+	
+	$server         = "localhost";
+    $user           = "root";
+    $pass           = "";
+    $dbname         = "brainhouse";
+	
+	
+	$verbindung = new mysqli($server, $user, $pass, $dbname);
+        if (mysqli_connect_errno())
+         {
+          echo "Keine Verbindung zum DBServer:" . mysqli_connect_error();
+         }
+		 
+	$ergebnis = mysqli_query($verbindung, "select * from kunde");
+         $stv=0;
+         while ($dsatz = mysqli_fetch_array($ergebnis, MYSQLI_ASSOC))
+           {If  ($email==$dsatz["EMail"])
+                 {
+                 //EMail schon vorhanden
+                 $id=$dsatz["O_ID"];
+                 $stv=1;
+                 }
+            }
+		if ($stv==0){
+				$eintragen = mysqli_query(&verbindung, "INSERT INTO kunde (Vorname, Nachname, Telefon, EMail, Strasse,HNR, O_ID, Passwort) VALUES ('$vorname', '$nachname', '','$email','','','','')");	 
+				 }
+			 
+         if ($stv==1){
+			 echo "<h1>Sie sind bereits für den Newsletter angemeldet</h1>";
+		 }
+	
 	
 ?>
 </body>
