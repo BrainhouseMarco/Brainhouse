3a8c352a54b94100325491e0c8d1c2f94c79b206
BrainhouseMarco <it15168@lehre.dhbw-stuttgart.de>
2016-07-12 15:37:39 +0200

    changed relative link
    link zur bestätigung geändert

--- php/BHweb_NewsletterAbo.php
+++ php/BHweb_NewsletterAbo.php
@@ -1,4 +1,4 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
+﻿<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
 <head>
 <title>Email Versendet</title>
 </head>
@@ -25,7 +25,7 @@
 	
 	echo "<h2>Empf&auml;nger:&nbsp" .$email. "</h2><br>";
 	echo "Sehr geehrte(r) Frau/Herr&nbsp;" .$vorname."&nbsp;". $nachname. ", <br>";
-	echo "um Ihre Anmeldung f&uuml;r unseren Newsletter abzuschlie&szlig;en, klicken sie bitte auf den folgenden Link: &nbsp <a href='bhwebhome.html'>link</a><br><br> Falls Sie Unseren Newsletter nicht abonniert haben sollten, ignorieren sie diese e-mail.<br>";
+	echo "um Ihre Anmeldung f&uuml;r unseren Newsletter abzuschlie&szlig;en, klicken sie bitte auf den folgenden Link: &nbsp <a href='../bhwebhome.html'>link</a><br><br> Falls Sie Unseren Newsletter nicht abonniert haben sollten, ignorieren sie diese e-mail.<br>";
 	echo "<br>";
 	echo "Mit freundlichen Gr&uuml;&szlig;en<br>";
 	echo "Ihr Brainhouse Team";
